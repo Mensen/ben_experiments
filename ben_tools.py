af9231cfd68e6fc5683bf343a571705ea5b7ed37
Armand Mensen <research.mensen@gmail.com>
2016-12-18 22:44:08 +0100

    added wait function and break function

--- ben_tools.py
+++ ben_tools.py
@@ -5,7 +5,7 @@ Created on Mon Dec 12 23:01:57 2016
 @author: mensen
 """
 
-from psychopy import event  # import some libraries from PsychoPy
+from psychopy import event, visual  # import some libraries from PsychoPy
 
 
 def waitForClick(myWin):
@@ -21,3 +21,40 @@ def waitForClick(myWin):
             return 1
         if mouse3:
             return 255
+            
+def takeABreak(myWin, pause_time):
+
+    message1 = visual.TextStim(win=myWin,
+           alignHoriz='center',
+           alignVert='center', 
+           color=(0, 0, 0),
+           colorSpace='rgb',
+           units='', 
+           pos=[0,0],
+           text='Zeit fuer eine Pause.')
+    
+    # put the message on the screen
+    message1.draw()   
+    myWin.flip()        
+
+    core.wait(pause_time)
+    
+def pushToContinue(myWin):
+    
+    message1 = visual.TextStim(win=myWin,
+           alignHoriz='center',
+           alignVert='center', 
+           color=(0, 0, 0),
+           colorSpace='rgb',
+           units='', 
+           pos=[0,0],
+           text='Druecken Sie eine Taste um fortzufahren.')
+    
+    # put the message on the screen
+    message1.draw()   
+    myWin.flip()        
+               
+    # wait for mousepress
+    button = waitForClick(myWin)
+    
+    
