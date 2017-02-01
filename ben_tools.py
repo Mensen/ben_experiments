23f2836e1c3164ebf1b09bf701d2a72050b06ae7
Armand Mensen <research.mensen@gmail.com>
2017-02-01 21:58:22 +0100

    completed alpha version of the random shape cancellation; renamed images to show compact difficulty level

--- ben_tools.py
+++ ben_tools.py
@@ -5,7 +5,7 @@ Created on Mon Dec 12 23:01:57 2016
 @author: mensen
 """
 
-from psychopy import event, visual  # import some libraries from PsychoPy
+from psychopy import event, visual, core  # import some libraries from PsychoPy
 import argparse
 
 
@@ -22,7 +22,43 @@ def waitForClick(myWin):
             return 1
         if mouse3:
             return 255
-            
+         
+def welcomeMessage(myWin, text):
+    event.Mouse(visible=False)
+
+    # welcome message
+    message1 = visual.TextStim(win=myWin,
+       alignHoriz='center',
+       alignVert='center', 
+       color=(0, 0, 0),
+       colorSpace='rgb',
+       units='', 
+       pos=[0,0],
+       text=text)
+       
+    # put the message on the screen
+    message1.draw()   
+    myWin.flip()                   
+    # wait for keypress    
+    core.wait(2)
+    
+    # welcome message
+    message1 = visual.TextStim(win=myWin,
+       alignHoriz='center',
+       alignVert='center', 
+       color=(0, 0, 0),
+       colorSpace='rgb',
+       units='', 
+       pos=[0,0],
+       text='Druecken Sie eine Taste um fortzufahren')
+       
+    # put the message on the screen
+    message1.draw()   
+    myWin.flip()
+             
+    # wait for mousepress
+    waitForClick(myWin)         
+         
 def takeABreak(myWin, pause_time):
 
     message1 = visual.TextStim(win=myWin,
