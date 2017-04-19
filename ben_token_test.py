b0153406cdef50d6a63f167976aa98e385dc35ea
Mensen <research.mensen@gmail.com>
2017-04-19 17:50:29 +0200

    completed version 1.0 of token test

--- ben_token_test.py
+++ ben_token_test.py
@@ -44,60 +44,101 @@ n_answers = [1, 1, 2, 2, 1, 1, 1, 2, 2, 2]
 
 
 # define color_scheme
-def defineColors():
+def defineColors(version):
    
     COLOR_BLUE = np.array([-1, -1, 1])
     COLOR_RED = np.array([1, -1, -1])
     COLOR_YELLOW = np.array([1, 1, -1])
     COLOR_GREEN = np.array([-1, 0.75, -1])
     
-    # pre-allocate color_scheme
-    color_scheme = np.ones((4, 5, 3))
-    
-    # all the blues
-    color_scheme[0,0] = COLOR_BLUE
-    color_scheme[1,1] = COLOR_BLUE
-    color_scheme[2,4] = COLOR_BLUE
-    color_scheme[3,3] = COLOR_BLUE
-    # all the reds
-    color_scheme[0,1] = COLOR_RED
-    color_scheme[1,2] = COLOR_RED
-    color_scheme[2,3] = COLOR_RED
-    color_scheme[3,0] = COLOR_RED 
-    # all the yellow
-    color_scheme[0,3] = COLOR_YELLOW 
-    color_scheme[1,0] = COLOR_YELLOW
-    color_scheme[2,1] = COLOR_YELLOW
-    color_scheme[3,4] = COLOR_YELLOW
-    # all the greens
-    color_scheme[0,4] = COLOR_GREEN
-    color_scheme[1,3] = COLOR_GREEN
-    color_scheme[2,2] = COLOR_GREEN
-    color_scheme[3,1] = COLOR_GREEN
+    if version is 1:
+        # pre-allocate color_scheme
+        color_scheme = np.ones((2, 5, 3))
+
+        # in row order
+        color_scheme[0, 0] = COLOR_YELLOW
+        color_scheme[0, 1] = COLOR_RED
+        color_scheme[0, 2] = COLOR_BLUE
+        color_scheme[0, 3] = COLOR_GREEN        
+        
+        color_scheme[1, 0] = COLOR_GREEN
+        color_scheme[1, 1] = COLOR_YELLOW
+        color_scheme[1, 3] = COLOR_BLUE
+        color_scheme[1, 4] = COLOR_RED
+               
+    elif version is 2:
+        # pre-allocate color_scheme
+        color_scheme = np.ones((4, 5, 3))
+        
+        # all the blues
+        color_scheme[0,0] = COLOR_BLUE
+        color_scheme[1,1] = COLOR_BLUE
+        color_scheme[2,4] = COLOR_BLUE
+        color_scheme[3,3] = COLOR_BLUE
+        # all the reds
+        color_scheme[0,1] = COLOR_RED
+        color_scheme[1,2] = COLOR_RED
+        color_scheme[2,3] = COLOR_RED
+        color_scheme[3,0] = COLOR_RED 
+        # all the yellow
+        color_scheme[0,3] = COLOR_YELLOW 
+        color_scheme[1,0] = COLOR_YELLOW
+        color_scheme[2,1] = COLOR_YELLOW
+        color_scheme[3,4] = COLOR_YELLOW
+        # all the greens
+        color_scheme[0,4] = COLOR_GREEN
+        color_scheme[1,3] = COLOR_GREEN
+        color_scheme[2,2] = COLOR_GREEN
+        color_scheme[3,1] = COLOR_GREEN
     
     return color_scheme
 
 # draw the shapes on screen
-def drawTokens():
+def drawTokens(version):
 
-    objects = [[0] * 5 for n in range(4)]
-   
+    row_range = [0]*2
+
+    if version is 1:
+        objects = [[0] * 5 for n in range(2)]
+        row_range[0] = range(0, 1)
+        row_range[1] = range(1, 2)
+        
+        circle_radius = 80
+        rectangle_height = 200
+
+    elif version is 2:
+        objects = [[0] * 5 for n in range(4)]
+        row_range[0] = range(0, 2)
+        row_range[1] = range(2, 4)
+        
+        circle_radius = 70
+        rectangle_height = 180
+
+
     # draw circles
-    for row in range(0, 2):
+    row_counter = 0
+    for row in row_range[0]:
+        
+        row_counter += 1        
+        
         for shape in range(0, 5):
         
             objects[row][shape] = visual.Circle(win=myWin,
                lineColor=background_color, 
                fillColor=color_scheme[row, shape],
                units='pix', 
-               pos=[object_x_centers[shape], object_y_centers[row]],
-               radius=70 / np.sqrt(row+1),
+               pos=[object_x_centers[shape], object_y_centers[version-1][row]],
+               radius= circle_radius / np.sqrt(row_counter),
                lineWidth=10)
                        
             objects[row][shape].setAutoDraw(True)
     
     # draw rectangles
-    for row in range(2, 4):
+    row_counter = 0
+    for row in row_range[1]:
+
+        row_counter += 1        
+        
         for shape in range(0, 5):
         
             # TODO: don't just guestimate the sizes
@@ -105,9 +146,9 @@ def drawTokens():
                lineColor=background_color, 
                fillColor=color_scheme[row, shape],
                units='pix', 
-               pos=[object_x_centers[shape], object_y_centers[row]],
-               width=60 / (row) * 4,
-               height=80 / (row) * 4,
+               pos=[object_x_centers[shape], object_y_centers[version-1][row]],
+               width= (rectangle_height * 0.75) / np.sqrt(row_counter),
+               height= rectangle_height / np.sqrt(row_counter),
                lineWidth=10)
                        
             objects[row][shape].setAutoDraw(True)
@@ -116,6 +157,13 @@ def drawTokens():
     core.wait(0.1)
     
     return objects
+
+def eraseTokens():
+    
+    for row in range(0, len(objects)):
+        for shape in range(0, 5):
+            
+            objects[row][shape].setAutoDraw(False)
 
 def playAudio():
     # open the audio file (n + 1 because of 0 indexing)
@@ -172,10 +220,12 @@ def runTrial(expected_answers):
             distance_to_shapes = object_x_centers - click_position[0]
             shape_index = (np.abs(distance_to_shapes)).argmin()
 
-            distance_to_rows = object_y_centers - click_position[1]
+            distance_to_rows = object_y_centers[version-1] - click_position[1]
             row_index = (np.abs(distance_to_rows)).argmin()
             
 #            print('distance to shape is %d %d') %(distance_to_shapes[shape_index], distance_to_rows[row_index])            
+#            print('shape_index is %d, row index is %d') %(shape_index, row_index)            
+                        
                         
             if abs(distance_to_shapes[shape_index]) < 80 and \
             abs(distance_to_rows[row_index]) < 80:
@@ -236,15 +286,20 @@ myWin = visual.Window([1000, 800], color=background_color, fullscr=1, monitor="t
 print('screen size is %d') %(myWin.size[0]) 
 
 # run the welcomeMessage function
-# ben_tools.welcomeMessage(myWin, 'Token Test')
+ben_tools.welcomeMessage(myWin, 'Token Test')
 
 # TODO: actually get screen size and adjust
 object_x_centers = np.linspace(-500, 500, num = 5)
-object_y_centers = np.linspace(-300, 300, num = 4)
+
+# Y centers change depending on question number
+object_y_centers = [0]*2
+object_y_centers[0] = np.linspace(-200, 200, num = 2)
+object_y_centers[1] = np.linspace(-300, 300, num = 4)
 
 # get the colorscheme and then draw the token
-color_scheme = defineColors()
-objects = drawTokens()
+version = 1
+color_scheme = defineColors(version)
+objects = drawTokens(version)
 
 # loop trials
 for n in range(0, options.num_trials):
@@ -256,7 +311,7 @@ for n in range(0, options.num_trials):
     if row_index is 100:
         break
     
-    # check if correct
+    # TODO: check if correct
     
     # record the results
     data_out.addData('question', n + 1)
@@ -267,11 +322,21 @@ for n in range(0, options.num_trials):
     
     data_out.nextEntry()    
 
+    # switch tokens after n = 3
+    if n is 3:
+        eraseTokens()
+        version = 2
+        color_scheme = defineColors(version)
+        objects = drawTokens(version)
 
 # print result of trial in the command line
 print('you hit row %d and shape %d') %(row_index + 1, shape_index + 1) 
 
 # quit the program
+eraseTokens()
+ben_tools.showEnd(myWin)
+ben_tools.waitForClick(myWin)
+
 core.wait(0.25)
 myWin.close()
 core.quit                   
